85eb24c965f0999763e3866ef2155d3d9e3b7af6
Riva <iso_on_fire@hotmail.com>
2025-05-15 05:40:51 +0200

    test(test): test

--- app/portfolio/src/main.tsx
+++ app/portfolio/src/main.tsx
@@ -13,6 +13,11 @@ console.warn("ENV", Object.entries(env).reduce((acc: Record<string, string | boo
   return acc;
 }, {}));
 
+console.warn("ENV", Object.entries(env).reduce((acc: Record<string, string | boolean>, [key, value]) => {
+  acc[key] = value;
+  return acc;
+}, {}));
+
 // Render the app
 const rootElement = document.getElementById("app");
 if (rootElement && !rootElement.innerHTML) {
