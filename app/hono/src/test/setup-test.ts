2d3bf894a048658c06adf7562ec0cc4d702f7e3a
Riva <iso_on_fire@hotmail.com>
2025-06-04 14:35:24 +0200

    ci(turbo): 🦊 CI turbo

--- app/hono/src/test/setup-test.ts
+++ app/hono/src/test/setup-test.ts
@@ -1,14 +1,8 @@
 // import { execSync } from "node:child_process";
-import fs from "node:fs";
-import { afterAll, beforeAll } from "vitest";
+// import fs from "node:fs";
+import { afterAll } from "vitest";
 
-beforeAll(async () => {
-  console.warn("Running migrations...");
-  // execSync("pnpm drizzle-kit push");
-});
-
-afterAll(async () => {
+afterAll(() => {
   console.warn("Cleaning up...");
-  console.warn("Cleaning up2...");
-  fs.rmSync("dev.database", { force: true });
+  // fs.rmSync("dev.database", { force: true });
 });
